1432917c146a3bdbba34e94c2e9eff2f3afb30c1
Chathura-Nilaksha <cnilakshami@gmail.com>
2025-01-26 18:41:59 +0530

    Developed chatBox component

--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Chat.css'
+import LeftSidebar from '../../components/leftSidebar/LeftSidebar'
+import ChatBox from '../../components/chatbox/chatbox'
+import RightSidebar from '../../components/rightSideBar/RightSidebar'
 
 const Chat = () => {
   return (
-    <div>
-      
+    <div className='chat'>
+      <div className="chat-container">
+        <LeftSidebar/>
+        <ChatBox/>
+        <RightSidebar/>
+      </div>      
     </div>
   )
 }
