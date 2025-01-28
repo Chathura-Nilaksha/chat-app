ada84110a60cb2dca7583648f8737e1d81ab7456
Chathura-Nilaksha <cnilakshami@gmail.com>
2025-01-28 18:43:26 +0530

    Developed Chatbox component

--- src/components/chatbox/ChatBox.jsx
+++ src/components/chatbox/ChatBox.jsx
@@ -10,6 +10,46 @@ const ChatBox = () => {
             <p>Ruchira Saman <img className='dot' src={assets.green_dot} alt="" /> </p>
             <img src={assets.help_icon} className='help' alt="" />
         </div>
+
+        <div className="chat-msg">
+
+            <div className="s-msg">
+                <p className="msg">Here-the sender's msg</p>
+                <div>
+                    <img src={assets.profile_img} alt="" />
+                    <p>2.30 PM</p>
+                </div>
+            </div>
+
+            <div className="s-msg">
+            <img className='msg-img' src={assets.pic1} alt="" />
+                <div>
+                    <img src={assets.profile_img} alt="" />
+                    <p>2.30 PM</p>
+                </div>
+            </div>
+
+
+
+            <div className="r-msg">
+                <p className="msg">Here-the receiver's msg</p>
+                <div>
+                    <img src={assets.profile_img} alt="" />
+                    <p>2.30 PM</p>
+                </div>
+            </div>
+
+
+
+
+        </div>
+
+        <div className="chat-input">
+            <input type="text" placeholder='Send a message' />
+            <input type="file" id='image' accept='image/png, image/jpeg' hidden/>
+            <lable htmlFor="image"><img src={assets.gallery_icon} alt="" /></lable>
+            <img src={assets.send_button} alt="" />
+        </div>
       
     </div>
   )
