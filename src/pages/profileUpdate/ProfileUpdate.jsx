72a6e0e093f9485c71f76010834a6c29eed41b4c
Chathura-Nilaksha <cnilakshami@gmail.com>
2025-02-07 21:37:31 +0530

    Developed profilepage component

--- src/pages/profileUpdate/ProfileUpdate.jsx
+++ src/pages/profileUpdate/ProfileUpdate.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './ProfileUpdate.css'
+import assets from '../../assets/assets'
 
 const ProfileUpdate = () => {
   return (
-    <div>
+    <div className='profile'>
+      <div className="profile-container">
+
+        <form >
+          <h3>Profile Detials</h3>
+          <label htmlFor="avatar">
+            <input hidden type="file" accept='.png, .jpg, .jpeg' id="avatar" />
+            <img src={assets.avatar_icon} alt="" />
+            Upload the Profile image
+          </label>
+
+          <input type="text" placeholder='Your Name' required/>
+          <textarea placeholder='Profile Bio' required ></textarea>
+          <button type='submit'>Save</button>
+
+        </form>
+
+        <img src={assets.logo_icon} alt="" />
+      </div>
       
     </div>
   )
